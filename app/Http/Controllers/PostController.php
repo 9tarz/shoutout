c51a9e67d959718ec29c061ff02f71435927c9f7
Kanatip Chitavisutthivong <kanatip.jit@gmail.com>
2015-11-28 16:13:08 +0700

    (PostController) can post with image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\Post;
 use App\Session;
-use App\Photo;
+use App\Image;
 
 class PostController extends Controller
 {
@@ -32,22 +32,56 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        // check who is token owner.
         $session = Session::where('token', $request->get('token'))->first(); //find user_id from token
         if($session == null) {
             $error = 1;
             $error_msg = "Token error!";
             return response()->json(['error' => $error, 'error_msg' => $error_msg]);
         } else {
+            // check user token isn't expired.
             if($session->is_expired == 0){
                 $error = 0;
-                $error_msg = "Shout completed. :)";
-                $post = new Post();
-                $post->user_id = $session->user_id;
-                $post->text = $request->get('text');
-                $post->latitude = $request->get('latitude');
-                $post->longitude = $request->get('longitude');
-                //$post->is_anonymous = $request->get('anonymous');
-                $post->save();
+                if ($request->file('image') == null || $request->file('image')->getClientSize() <= 0) {
+                    $error = 1;
+                    $error_msg = "Image not found! ";
+                }
+                else {
+                    // upload image to imgur.com
+                    $result = $this->upload_image($request->file('image'));
+                    //echo $result;
+                    $result = json_decode($result);
+
+                    // check upload image to imgur.com isn't failed.
+                    if ($result->success != false) {
+
+                        // store image information.
+                        $image = new Image();
+                        $image->imgur_url = $result->data->link;
+                        $image->post_id = null;
+                        $error_msg = "Upload completed";
+                        $image->save();
+
+                        // store the text post.
+                        $post = new Post();
+                        $post->user_id = $session->user_id;
+                        $post->text = $request->get('text');
+                        $post->latitude = $request->get('latitude');
+                        $post->longitude = $request->get('longitude');
+                        //$post->is_anonymous = $request->get('anonymous');
+                        $post->save();
+
+                        // update post id of image.
+                        $image->post_id = $post->id;
+                        $image->save();
+
+                        $error_msg = "Shout completed. :)";
+                    } else {
+                        $error = 1;
+                        // show error msg from imgur.com API
+                        $error_msg = $result->data->error; 
+                    }
+                }
                 return response()->json(['error' => $error, 'error_msg' => $error_msg]);
             } else {
                 $error = 1;
@@ -98,28 +132,21 @@ class PostController extends Controller
     }
 
     /**
-     * upload image to imgur.com
+     * function upload image to imgur.com
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Symfony\Component\HttpFoundation\File\UploadedFile  $image_request
+     * @return string $reply
      */
-    public function upload_image(Request $request)
+    private function upload_image($image_request)
     {
-        /*if (empty($request->get('clientid')) || $request->file('upload');$_FILES['upload']['error'] !== 0 || $_FILES['upload']['size'] > 5000000000) {
-            exit;
-        }*/
         $error_msg = "";
         $client_id = "68a375e185f3ffb";
+        $reply = "";
 
-        if ($request->file('image') == null)
+        if ($image_request == null) {
             $error_msg = "null"; 
-
-        /*if ($input_image->getMimeType() != 'image') {
-            die('Invalid image type');
-        }*/
-
-        $image = file_get_contents($request->file('image')->getRealPath());
-
+        }
+        $image = file_get_contents($image_request->getRealPath());
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
         curl_setopt($ch, CURLOPT_POST, TRUE);
@@ -132,8 +159,49 @@ class PostController extends Controller
 
         curl_close($ch);
 
-        $reply = json_decode($reply);
+        //$reply = json_decode($reply);
 
+        return $reply;
+
+    }
+
+    /**
+     * upload image to imgur.com for test
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function upload_image_old(Request $request)
+    {
+        $error_msg = "";
+        $client_id = "68a375e185f3ffb";
+        $reply = "";
+
+        if ($request->file('image') == null) {
+            $error_msg = "null"; 
+        }
+        /*if ($request->file('image')->getMimeType() != 'image') {
+            $error_msg = "Invalid image type";
+        */
+        //else {
+            $image = file_get_contents($request->file('image')->getRealPath());
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Client-ID $client_id" ));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, array( 'image' => base64_encode($image) ));
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+            $reply = curl_exec($ch);
+
+            curl_close($ch);
+
+            $reply = json_decode($reply);
+
+        //}
+
+        //return $reply;
         return response()->json(['reply' => $reply, 'error_msg' => $error_msg]);
 
     }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,13 +6,13 @@
   </head>
   <body>
   
-				<form action="{{ route('post') }}" method="POST">
+				<form action="{{ route('post') }}" method="POST" enctype="multipart/form-data">
 					<input type="text" placeholder='token' name="token"><br>
 					<input type="text" placeholder="text" name="text"><br>
 					<input type="text" placeholder="latitude" name="latitude"><br>
 					<input type="text" placeholder="longitude" name="longitude"><br>
 					<input type="int" placeholder="anonymous" name="anonymous"><br>
-					
+					Image <input type="file" name="image" /><br/>
 					<input type = "submit" value = "SHOUT" /><br>
 					
 				</form>
